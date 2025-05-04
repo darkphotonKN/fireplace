a3a4ea2bb64003dbc77a7314b74d875102a9d021
Kranti <darkphoton20@gmail.com>
2025-05-04 15:10:19 +0800

    Adding cors handling for router during dev.

--- config/routes.go
+++ config/routes.go
@@ -1,10 +1,13 @@
 package config
 
 import (
+	"fmt"
+
 	"github.com/darkphotonKN/fireplace/internal/ai"
 	"github.com/darkphotonKN/fireplace/internal/checklistitems"
 	"github.com/darkphotonKN/fireplace/internal/insights"
 	"github.com/darkphotonKN/fireplace/internal/user"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,6 +16,20 @@ import (
 **/
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
+
+	// NOTE: debugging middleware
+	router.Use(func(c *gin.Context) {
+		fmt.Println("Incoming request to:", c.Request.Method, c.Request.URL.Path, "from", c.Request.Host)
+		c.Next()
+	})
+
+	// TODO: CORS for development, remove in PROD
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PATCH", "OPTIONS"},
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+	}))
 
 	// base route
 	api := router.Group("/api")
